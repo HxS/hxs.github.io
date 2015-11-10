fb0744eafadcee2dfe5bfdfdc4a684c38af58092
izumin5210 <masayuki@izumin.info>
2015-11-10 10:14:55 +0900

    Add {build,clean,watch,serve,deploy} tasks and register them as npm scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import path         from "path";
 import cssnano      from "cssnano";
 import immutableCss from "immutable-css";
 import loadPlugins  from "load-plugins";
+import del          from "del";
 
 const $g = loadPlugins("gulp-*", { strip: "gulp" });
 const $p = loadPlugins("postcss-*");
@@ -10,11 +11,22 @@ const $p = loadPlugins("postcss-*");
 
 // configurations --------------------------------
 const SRC_DIR       = path.join(__dirname, "./assets");
-const DEST_DIR      = path.join(__dirname, "./static/assets");
+const TEMP_DIR      = path.join(__dirname, "./static/assets");
+const DEST_DIR      = path.join(__dirname, "./public");
 
 const STYLES_DIR    = path.join(SRC_DIR, "styles");
 
 const CSS_ENTRYPOINTS = ["style.css"].map(filename => path.join(STYLES_DIR, filename));
+
+const CLEAN_TARGET_DIRS = [
+  TEMP_DIR,
+  path.join(DEST_DIR, "*"),
+  `!${path.join(DEST_DIR, ".git")}`
+];
+
+const DEPLOY_OPTIONS = {
+  branch: "master"
+};
 
 
 // styles --------------------------------
@@ -36,8 +48,26 @@ gulp.task("build:css", () => {
     .pipe($g.postcss(processors))
     .pipe($g.sourcemaps.write("."))
     .pipe($g.size())
-    .pipe(gulp.dest(DEST_DIR));
+    .pipe(gulp.dest(TEMP_DIR));
+});
+
+gulp.task("watch:css", () => {
+  gulp.watch(path.join(STYLES_DIR, "**/*.css"), ['build:css']);
 });
 
 
-gulp.task("build", ["build:css"]);
+// misc --------------------------------
+gulp.task("clean", () => del(CLEAN_TARGET_DIRS, {dot: true}))
+
+gulp.task("build:hugo", $g.shell.task("hugo"))
+gulp.task("serve", $g.shell.task("hugo server"))
+gulp.task("watch:hugo", $g.shell.task("hugo server -ws ."))
+
+gulp.task("build", ["build:hugo", "build:css"]);
+
+gulp.task("watch", ["watch:css", "watch:hugo"]);
+
+gulp.task("deploy", ["build"], () => {
+  return gulp.src(path.join(DEST_DIR, "**/*"))
+    .pipe($.ghPages(DEPLOY_OPTIONS));
+});
