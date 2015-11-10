c83bce2abe6b0461f29a992d1c1271fe07c902e3
izumin5210 <masayuki@izumin.info>
2015-11-10 13:16:20 +0000

    Update 2015-11-10T13:16:19.356Z

--- scripts/install_hugo.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-mkdir -v $TRAVIS_BUILD_DIR/bin
-mv -v hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $TRAVIS_BUILD_DIR/bin/hugo
-rm -v -rf hugo_${HUGO_VERSION}_linux_amd64*
-chmod -v +x $TRAVIS_BUILD_DIR/bin/hugo
